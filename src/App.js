829b92ebe6bc3de94c8d0052e899f94abe6b6741
Julien Rousseau <julienrousseau16@gmail.com>
2020-10-02 16:18:32 +0200

    dev branch created + router initialized

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+import Landing from './components/Landing'
+import MainContainer from './components/MainContainer'
 
 import './App.css'
 
 const App = () => {
   return (
     <div className='App'>
-      <img className='App_logo' src={require('./pictures/logo.png')} alt='main-logo' />
+      <Switch>
+        <Route exact path='/'>
+          <Landing />
+        </Route>
+        <Route path='/main'>
+          <MainContainer />
+        </Route>
+      </Switch>
+
     </div>
   )
 }
