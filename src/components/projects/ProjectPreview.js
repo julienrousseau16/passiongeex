d7dfd76c3db04b3117c0db050828d2363484fcc6
Julien Rousseau <julienrousseau16@gmail.com>
2020-11-13 17:24:33 +0100

    profile + projects sections now css-fit the tablet format

--- src/components/projects/ProjectPreview.js
+++ src/components/projects/ProjectPreview.js
@@ -7,7 +7,7 @@ const ProjectPreview = ({ project }) => {
 
   return (
     <div className='ProjectPreview'>
-      <NavLink to={{ pathname: `/home/project/${project.slug}`, project: project }}><img src={require('../../pictures/' + project.mobilepic)} alt='project preview' /></NavLink>
+      <NavLink to={{ pathname: `/home/project/${project.slug}`, project: project }}><img style={project.slug === 'passiongeex' ? {border: '1px solid grey'}: null} src={require('../../pictures/' + project.mobilepic)} alt='project preview' /></NavLink>
     </div>
   )
 }
